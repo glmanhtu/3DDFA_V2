3acf807faca800a13a52e05f22df7efb8fcce478
VU Manh Tu <glmanhtu@gmail.com>
2022-10-17 00:51:48 +0200

    Fix path

--- src/tddfa/utils/config.py
+++ src/tddfa/utils/config.py
@@ -17,9 +17,15 @@ class LandmarkDetectorConfig:
     def load_config_file(self, file_path):
         self.config = yaml.load(open(file_path), Loader=yaml.SafeLoader)
 
+    def update_abs_path(self, key):
+        current_path = self.config[key]
+        self.config[key] = get_abs_path(*current_path.split("/"))
+
     @staticmethod
     def default_config(name="mb1_120x120.yml"):
         config_file = get_abs_path("configs", name)
         config = LandmarkDetectorConfig()
         config.load_config_file(config_file)
+        config.update_abs_path("checkpoint_fp")
+        config.update_abs_path("bfm_fp")
         return config
